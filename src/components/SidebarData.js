d57274dd6e3fa913845c3564711693b6a1169b8e
Frans Mazhar <frans.mazhar@mii.co.id>
2022-05-04 11:32:12 +0700

    update

---
+++ src/components/SidebarData.js
@@ -0,0 +1,22 @@
+import {} from '@mui/icons-material';
+
+export const SidebarData = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: '<CloseIcon/>',
+        cName: 'nav-text'
+    },
+    {
+        title: 'Report',
+        path: '/reports',
+        icon: '',
+        cName: 'nav-text'
+    },
+    {
+        title: 'Product',
+        path: '/product',
+        icon: '',
+        cName: 'nav-text'
+    },
+]
